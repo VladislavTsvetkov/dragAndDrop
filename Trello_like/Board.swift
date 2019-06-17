f8c20ddddc1033009bc0acfad2216f690afc33e5
Цветков Владислав Дмитриевич <Tsvetkov.V.Dm@sberbank.ru>
2019-06-17 15:26:15 +0300

    Драг и дроп реализован

--- Trello_like/Board.swift
+++ Trello_like/Board.swift
@@ -1,20 +1,20 @@
 //
-//  ViewController.swift
+//  Board.swift
 //  Trello_like
 //
 //  Created by Цветков Владислав Дмитриевич on 17/06/2019.
 //  Copyright © 2019 Цветков Владислав Дмитриевич. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class Board: Codable {
+    
+    var title: String
+    var items: [String]
+    
+    init(title: String, items: [String]) {
+        self.title = title
+        self.items = items
     }
-
-
 }
-
