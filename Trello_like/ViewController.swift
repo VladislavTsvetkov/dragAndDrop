d411d16e68358fc53d0c178ce25a7e7dbd3fa7f2
Цветков Владислав Дмитриевич <Tsvetkov.V.Dm@sberbank.ru>
2019-06-17 12:40:42 +0300

    Initial Commit

---
+++ Trello_like/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trello_like
+//
+//  Created by Цветков Владислав Дмитриевич on 17/06/2019.
+//  Copyright © 2019 Цветков Владислав Дмитриевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
